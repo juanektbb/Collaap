556e19916a47b942f8719a27c3da16b928165256
Juan D. Diaz <inf.juandiaz@gmail.com>
2020-08-29 14:37:58 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import React, {Component} from 'react';
+import {
+  Text,
+} from 'react-native';
+
+import Home from './src/screens/Home.js'
+
+class App extends Component<Props>{
+  render(){
+    return(
+      <Home />
+      )
+  }
+}
+
+export default App;
